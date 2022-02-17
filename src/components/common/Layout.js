5c1ab9f6edcab219f28e7dbd24574126b779633f
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2022-02-17 20:11:37 +0000

    zecloud icon

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -119,7 +119,7 @@ const DefaultLayoutSettingsQuery = props => (
                         }
                     }
                 }
-                file(relativePath: {eq: "ghost-icon.png"}) {
+                file(relativePath: {eq: "zecloud-icon.png"}) {
                     childImageSharp {
                         fixed(width: 30, height: 30) {
                             ...GatsbyImageSharpFixed
